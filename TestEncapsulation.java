95dfb09d19bc211fe82eaac1c72dc760a3e47407
NikitaGawande123 <96858213+NikitaGawande123@users.noreply.github.com>
2022-01-03 19:18:46 +0530

    Add files via upload

---
+++ TestEncapsulation.java
@@ -0,0 +1,19 @@
+package PracticeUdemyEncapsulation;
+
+public class TestEncapsulation {
+	
+	
+		public static void main(String[] args) {  
+		 
+		    Account acc=new Account();  
+		   
+		    acc.setAcc_no(7560504000L);  
+		    acc.setName("Nikita Gawande");  
+		    acc.setEmail("gawanden881@gmail.com");  
+		    acc.setAmount(500000f);  
+		    
+		    System.out.println(acc.getAcc_no()+" "+acc.getName()+" "+acc.getEmail()+" "+acc.getAmount());  
+		}  
+		}  
+
+
